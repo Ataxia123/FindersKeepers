a896399e5b814742fd143b1231e6e64f8d886c11
Ataxia123 <vp.ca94@gmail.com>
2022-08-08 20:20:17 -0500

    stuff

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -1,25 +1,21 @@
-import React from "react";
+import React from 'react';
 
 // displays a page header
 
-export default function Header({link, title, subTitle}) {
+export default function Header({ link, title, subTitle }) {
   return (
     <div>
       <a href={link} target="_blank" rel="noopener noreferrer">
-        <h2 className="text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-          {title}
-        </h2>
+        <img src={'https://i.imgur.com/7gTbXRd.jpg'} alt="profile" />
+        <h2 className="text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{title}</h2>
       </a>
-      <p className="text-sm font-normal italic text-gray-500 dark:text-slate-100 tracking-wide">
-        {subTitle}
-      </p>
+      <p className="text-sm font-normal italic text-gray-500 dark:text-slate-100 tracking-wide">{subTitle}</p>
     </div>
   );
 }
 
-
 Header.defaultProps = {
-  link: "https://github.com/scaffold-eth/scaffold-eth",
-  title: "🏗 scaffold-eth",
-  subTitle: "forkable Ethereum dev stack focused on fast product iteration",
-}
+  link: 'https://github.com/Ataxia123/scaffold-eth-tailwind',
+  title: 'Finders Keepers',
+  subTitle: 'Enjoying Good Art',
+};
